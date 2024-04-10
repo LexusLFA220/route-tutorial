525c9b377fe6b98ca7040a87fc6d73aea4a13c75
LexusLFA220 <alex.amin@lc.cuny.edu>
2024-04-10 17:56:28 -0400

    Added Styling and better Presentation to index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,25 @@
 export default function Index() {
     return (
       <p id="zero-state">
-        This is a demo for React Router.
+       <h1>Welcome to Contact Manager</h1> 
         <br />
+        You Can Add and Manage All of Your Contacts Including Yourself
+        <br />
+        <h3>How to add contact Contact</h3>
+        <p>Simply Click New Button Fill out the Form and Save Easy Peasy. </p>
+        <h4>Want to Find out How the Website Works?</h4>
         Check out{" "}
         <a href="https://reactrouter.com">
           the docs at reactrouter.com
         </a>
+        <br/>
+        and 
+        <br/>
+        Check out{" "}
+        <a href=" https://reactrouter.com/en/main/start/tutorial">
+          the tutorial at reactrouter.com
+        </a>
+        
         .
       </p>
     );
